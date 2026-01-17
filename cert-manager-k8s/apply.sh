5200f71cc75f2763a651a1097cb0ea6f3147dc23
Mariusz Jasinski <Mariusz.Jasinski@asseco.pl>
2026-01-17 22:31:42 +0100

    apply.sh

--- cert-manager-k8s/apply.sh
+++ cert-manager-k8s/apply.sh
@@ -3,9 +3,9 @@ kubectl create namespace cert-manager || exit 1
 
 echo
 echo "cert-manager"
-sleep 2
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.1/cert-manager.yaml
 
 echo
 echo "internal-ca"
+sleep 2
 kubectl apply -f internal-ca.yaml
